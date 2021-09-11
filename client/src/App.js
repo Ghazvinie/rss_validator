98e656cc89822076c15fb37b1f63db226e038224
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-11 15:36:45 +0100

    updated App

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,15 @@ import './App.css';
 import FormPage from './components/FormPage';
 
 function App() {
-
-  useEffect(() => {
-
-    async function callApi() {
-      const res = await fetch('/api');
-      const data = await res.json();
-    };
-    callApi();
-  }, []);
-
   return (
     <>
       <main>
         <h1>SpeechKit RSS Validator</h1>
-        <h2>Check if your feed is valid:</h2>
         <p>
-          (The feed requirements can be found <a href='https://docs.speechkit.io/docs/rss-feed'>here</a>. 
-          On submitting an URL you will be alerted to any elements that are missing)
+          The feed requirements can be found <a href='https://docs.speechkit.io/docs/rss-feed'>here</a>. 
+          On submitting an URL you will be alerted to any elements that are missing.
         </p>
+        <h2>Check if your feed is valid:</h2>
         <FormPage />
       </main>
     </>
