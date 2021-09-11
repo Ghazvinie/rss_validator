479f0be0f05a2da5b39d2543df4e46592d84a3de
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-11 15:52:53 +0100

    added some comments

--- api/routes/api.js
+++ api/routes/api.js
@@ -11,11 +11,12 @@ router.post('/', async (req, res) => {
   const testProperties = ['author', 'content', 'description', 'enclosure', 'guid', 'link', 'pubDate', 'title', 'url'];
   const results = {};
 
+  // Get data from url
   const getData = async (url) => {
     try {
-      const response = await axios.get(url)
+      const response = await axios.get(url);
       return response;
-    } catch (error) {
+    } catch (error) { // Unable to get response from url
       results.url = false;
       res.send(results);
       return;
@@ -23,25 +24,25 @@ router.post('/', async (req, res) => {
   };
 
   try {
-    const { data } = await getData(url);
-    const json = parser.toJson(data, { object: true });
-    const { item } = json.rss.channel;
-  
+    const { data } = await getData(url); 
+    const json = parser.toJson(data, { object: true }); // Convert xml to json
+    const { item } = json.rss.channel; // Specify item property
+
+    // Map through test properties and check against first index in items array
     testProperties.map(testItem => {
       if (item[0][testItem]){
         results[testItem] = true;
       } else {
         results[testItem] = false;
       }
-      results.url = true;
+      results.url = true; // Url is valid and working, above .map() will look for url element which is not what is needed
     });
 
   } catch (error) {
     console.log(error)
     return;
   }
-
-  res.send(results);
+  res.send(results); // Send results object to client
 });
 
 
--- client/src/components/FormPage.js
+++ client/src/components/FormPage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import validator from 'validator';
 import '../FormPage.css';
 import { requirementsObj } from '../requirements';
-import {ImSpinner6} from 'react-icons/im'
+import { ImSpinner6 } from 'react-icons/im'
 
 import ElementDescriptions from './ElementDescriptions';
 
@@ -13,17 +13,20 @@ function FormPage() {
     const [urlError, setUrlError] = useState(null);
     const [isParsing, setIsParsing] = useState(false);
 
+    // Handle text input
     const handleChange = (e) => {
         const value = e.target.value;
         setTextInput(value);
     };
 
+    // Handles submit actions
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setUrlError(null);
-        setTests(requirementsObj);
+        setUrlError(null); // Reset url error
+        setTests(requirementsObj); // Reset tests
         setIsParsing(true);
 
+        // Check url is formatted correctly
         const result = validator.isURL(textInput.trim());
         if (!result) {
             setUrlError('Invalid URL');
@@ -31,6 +34,7 @@ function FormPage() {
             return;
         };
 
+        // Send url to backend and await response
         const res = await fetch('/api', {
             method: 'POST',
             headers: {
@@ -40,8 +44,9 @@ function FormPage() {
                 url: textInput.trim()
             })
         });
+        
+        // Get data from response and update test items pass statuses to match response object
         const data = await res.json();
-
         for (const item in data) {
             setTests(prevTests => ({
                 ...prevTests,
@@ -54,57 +59,54 @@ function FormPage() {
         setIsParsing(false);
     };
 
-
-
-
     return (
         <div className='form-table-container'>
 
             <div className='form-container'>
                 <form onSubmit={(e) => handleSubmit(e)}>
-
                     <input
                         type='text'
                         value={textInput}
                         onChange={(e) => handleChange(e)}
                         placeholder='Please input a valid url'
                     />
-                    <button>{isParsing ? <ImSpinner6 className='spin-icon'/> : 'Submit'}</button>
+                    <button>{isParsing ? <ImSpinner6 className='spin-icon' /> : 'Submit'}</button>
                 </form>
                 <span className='url-error'>{urlError}</span>
             </div>
+
             <div className='table-container'>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Status</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(tests).map(singleTest => {
-                        return (
-                            <tr key={tests[singleTest].title}>
-                                <td className='dotTd'>
-                                    <span className="dot"
-                                        style={{
-                                            backgroundColor: tests[singleTest].passStatus === 'none' ? 'rgba(59, 59, 59, 0.255)' : tests[singleTest].passStatus ?
-                                                'rgba(63, 213, 63, 0.555)' : 'rgba(255, 0, 64, 0.555)'
-                                        }}>
-                                    </span>
-                                </td>
-                                <td>
-                                    <p className='title' >{tests[singleTest].title}</p>
-                                    <ElementDescriptions
-                                        passStatus={tests[singleTest].passStatus}
-                                        startDescription={tests[singleTest].startDescription}
-                                        failDescription={tests[singleTest].failDescription} />
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Status</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.keys(tests).map(singleTest => {
+                            return (
+                                <tr key={tests[singleTest].title}>
+                                    <td className='dotTd'>
+                                        <span className="dot"
+                                            style={{
+                                                backgroundColor: tests[singleTest].passStatus === 'none' ? 'rgba(59, 59, 59, 0.255)' : tests[singleTest].passStatus ?
+                                                    'rgba(63, 213, 63, 0.555)' : 'rgba(255, 0, 64, 0.555)'
+                                            }}>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <p className='title' >{tests[singleTest].title}</p>
+                                        <ElementDescriptions
+                                            passStatus={tests[singleTest].passStatus}
+                                            startDescription={tests[singleTest].startDescription}
+                                            failDescription={tests[singleTest].failDescription} />
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
 
         </div >
