819570657a65d25f22e011087b3c9ea74219acc3
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-14 19:08:40 +0100

    trying again

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(express.static(__dirname, '..client/build'));
+app.use(express.static(__dirname, '..client/build'));
 
 app.use('/api', apiRouter);
 
