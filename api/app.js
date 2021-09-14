1606136f94ca2e9f336e6f22e2a495ec98dd3831
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-14 18:13:55 +0100

    some reshuffle of repo

--- api/app.js
+++ api/app.js
@@ -1,44 +1,23 @@
-const createError = require('http-errors');
 const express = require('express');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-// view engine setup
-app.set('views', 'views');
-app.set('view engine', 'jade');
+const PORT = process.env.PORT || 5000;
 
-app.use(logger('dev'));
-app.use(express.json());
-// app.use(express.static(__dirname, '..client/build'));
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json()) // To parse the incoming requests with JSON payloads
+app.use(express.json());
 
-app.use('/', indexRouter);
+app.use(express.static(__dirname, '..client/build'));
+
 app.use('/api', apiRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname, '../client/build','index.js' );
 });
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
-
 
 module.exports = app;
